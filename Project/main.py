db9016c392a5b068b7570bfd23278a44ac07b7de
Lucian <l.diaconu@nexidigital.eu>
2023-04-26 14:15:47 +0200

    Cleaned code, converted pdf to html

--- Project/main.py
+++ Project/main.py
@@ -28,75 +28,37 @@ class Cell:
         else:
             return [len(self.pride)/10*255,len(self.herd)/10*255,self.vegetobDensity/100*255]
 
-#main 
 def main():
-    #create a cell grid of NUMCELLS x NUMCELLS using numpy full of cell objects each with a random vegetobDensity using full_like
     cell_grid = np.empty((planisuss_constants.NUMCELLS,planisuss_constants.NUMCELLS),dtype=Cell)
 
     for i in range(planisuss_constants.NUMCELLS):
         for j in range(planisuss_constants.NUMCELLS):
-            #make the border cells water (-1) and the rest land (0-100)
             if i == 0 or i == planisuss_constants.NUMCELLS-1 or j == 0 or j == planisuss_constants.NUMCELLS-1:
                 cell_grid[i][j] = Cell(i,j,"water",-1)
             else:
                 cell_grid[i][j] = Cell(i,j,"land",np.random.randint(0,100),np.random.randint(0,10),np.random.randint(0,10))
-            
-    #map cell_grid every cell to a rgb value numpy map
-    # rgb_grid = np.empty((planisuss_constants.NUMCELLS,planisuss_constants.NUMCELLS,3))
-    # for i in range(planisuss_constants.NUMCELLS):
-    #     for j in range(planisuss_constants.NUMCELLS):
-    #         rgb_grid[i][j] = cell_grid[i][j].Rgb()
     
-    
-    # rgb_grid = np.array([[cell_grid[i][j].RGB() for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
 
-    # plt.imshow(rgb_grid.astype(np.uint8))
-    # plt.colorbar()
- 
-    # plt.show()
-
-    #update the vegetobDensity of each cell
     for i in range(planisuss_constants.NUMCELLS):
         for j in range(planisuss_constants.NUMCELLS):
             cell_grid[i][j].vegetobDensity += planisuss_constants.GROWING
             if cell_grid[i][j].vegetobDensity > 100:
                 cell_grid[i][j].vegetobDensity = 100
     
-    
-    # def update_plot():
-    #     for i in range(planisuss_constants.NUMCELLS):
-    #         for j in range(planisuss_constants.NUMCELLS):
-    #             cell_grid[i][j].vegetobDensity += planisuss_constants.GROWING
-    #             if cell_grid[i][j].vegetobDensity > 100:
-    #                 cell_grid[i][j].vegetobDensity = 100
-    #     im.set_data(np.array([[cell_grid[i][j].RGB() for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)]))
-    #     return im
-    
     def update(frame):
         for i in range(planisuss_constants.NUMCELLS):
             for j in range(planisuss_constants.NUMCELLS):
                 cell_grid[i][j].vegetobDensity = np.random.randint(0,100)
                 cell_grid[i][j].herd = np.empty(np.random.randint(0,10),dtype=object)
-                cell_grid[i][j].pride = np.empty(np.random.randint(0,10),dtype=object)
-                
+                cell_grid[i][j].pride = np.empty(np.random.randint(0,10),dtype=object)        
         im.set_data(np.array([[cell_grid[i][j].RGB() for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)]).astype(np.uint8))
         return [im]
 
     rgb_grid = np.array([[cell_grid[i][j].RGB() for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
-    #animate plot
-    # plt.imshow(updated_rgb_grid.astype(np.uint8))
     fig, ax = plt.subplots()
-
-# Display the matrix as an image
     im = ax.imshow(rgb_grid)
-
-
-    anim = animation.FuncAnimation(fig, update, frames=100, interval=100, blit=True)
-
+    animation.FuncAnimation(fig, update, frames=100, interval=100, blit=True)
     plt.show()
    
-
-        
-
 if __name__ == "__main__":
     main()
