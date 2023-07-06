1928acf8c38cc153cf2438ce575cd4d2f1e09b2a
Lucian <l.diaconu@nexidigital.eu>
2023-07-06 11:19:04 +0200

    Code refactoring

--- Project/Cells.py
+++ Project/Cells.py
@@ -7,24 +7,27 @@ class Cell:
         self.vegetobDensity = vegetobDensity # 0-100
         self.herd = np.empty(0,dtype=Erbast)
         self.pride = np.empty(0,dtype=Carviz)
-    #when cast to int retun the vegetobDensity
-    # def __int__(self):
-    #     return np.dstack((self.vegetobDensity,len(self.herd),len(self.pride)))
-    
+
     def __str__(self):
         return f"{self.x},{self.y}-{len(self.herd)}"
     
     def __int__(self):
         return self.vegetobDensity
-
+    
+    def Vegetob(self):
+        return self.vegetobDensity/100*255
+    def Erbast(self):
+        return len(self.herd)/10*255
+    def Carviz(self):
+        return len(self.pride)/10*255
+    
     def RGB(self):
         if self.cell_type == "water":
             return [0,0,255]
         else:
-            return [len(self.pride)/10*255,len(self.herd)/10*255,self.vegetobDensity/100*255]
-    def Green(self):
-        return self.vegetobDensity/100*255
-        
+            return [self.Carviz(),self.Erbast(),self.Vegetob()]
+
+
 class Erbast:
     def __init__(self,energy,lifetime,age,socialAttitude):
         self.energy = energy 
@@ -36,5 +39,4 @@ class Carviz:
     def __init__(self,energy,lifetime,age,socialAttitude):
         self.energy = energy 
         self.lifetime = lifetime 
-        self.age = age # 0-lifetime
-        self.socialAttitude = socialAttitude # 0-1
+        self.socialAttitude = socialAttitude
--- Project/main.py
+++ Project/main.py
@@ -2,83 +2,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import planisuss_constants
-from Cells import Cell
-from Cells import Erbast
-from Cells import Carviz
+from planisuss_constants import NUMCELLS, NEIGHBORHOOD, GROWING
+from Cells import Cell, Erbast, Carviz
 from matplotlib.widgets import Button
 from perlin_noise import PerlinNoise
 import random
 
 
-# Create the cell grid and the noise map for the vegetob density
-cell_grid = np.empty((planisuss_constants.NUMCELLS,planisuss_constants.NUMCELLS),dtype=Cell)
-noise = PerlinNoise(octaves=planisuss_constants.NUMCELLS/10, seed=np.random.randint(0,100))
-pix = planisuss_constants.NUMCELLS
-noise_map = [[int((noise([i/pix, j/pix])+1)*50) for j in range(pix)] for i in range(pix)]
+# FUNCTIONS
+def createGrid(numCells):
+    return np.empty((NUMCELLS,NUMCELLS),dtype=Cell)
 
+def getNoiseMap():
+    noise = PerlinNoise(octaves=NUMCELLS/10, seed=np.random.randint(0,100))
+    pix = NUMCELLS
+    noise_map = [[int((noise([i/pix, j/pix])+1)*50) for j in range(pix)] for i in range(pix)]
+    return noise_map
 
-# Populate the cell grid with cells
-for i in range(planisuss_constants.NUMCELLS):
-    for j in range(planisuss_constants.NUMCELLS):
-        if i == 0 or i == planisuss_constants.NUMCELLS-1 or j == 0 or j == planisuss_constants.NUMCELLS-1:
-            cell_grid[i][j] = None
-        else:
-            cell_grid[i][j] = Cell(i,j,"land",noise_map[i][j])
-            #for each cell assign a herd of erbast
-            cell_grid[i][j].herd = [Erbast(5,50,0,random.choice([0,1]) ) for _ in range(0,random.randint(0,15))]
-            cell_grid[i][j].pride = [Carviz(5,50,0,random.choice([0,1]) ) for _ in range(0,random.randint(0,15))]
-                       
+def populateGrid(grid):
+    noise_map = getNoiseMap()
+    for i in range(NUMCELLS):
+        for j in range(NUMCELLS):
+            if i == 0 or i == NUMCELLS-1 or j == 0 or j == NUMCELLS-1:
+                grid[i][j] = None
+            else:
+                grid[i][j] = Cell(i,j,"land",noise_map[i][j])
+                #for each cell assign a herd of erbast
+                grid[i][j].herd = [Erbast(5,50,0,random.choice([0,1]) ) for _ in range(0,random.randint(0,15))]
+                grid[i][j].pride = [Carviz(5,50,0,random.choice([0,1]) ) for _ in range(0,random.randint(0,15))]
 
+def getRgbGtidByCellType(grid, cellType):
+    if(cellType == "vegetob_grid"):
+        return np.array([[grid[i][j].vegetobDensity if grid[i][j] is not None else 0 for j in range(NUMCELLS)] for i in range(NUMCELLS)])
+    elif(cellType == "erbast_grid"):
+        return np.array([[len(grid[i][j].herd) if grid[i][j] is not None else 0 for j in range(NUMCELLS)] for i in range(NUMCELLS)])
+    elif(cellType == "carviz_grid"):
+        return np.array([[len(grid[i][j].pride) if grid[i][j] is not None else 0 for j in range(NUMCELLS)] for i in range(NUMCELLS)])
+    else:
+        return np.array([[grid[i][j].RGB() if grid[i][j] is not None else [0,0,0] for j in range(NUMCELLS)] for i in range(NUMCELLS)]).astype(np.uint8)
 
+#INITIALIZATION
+cell_grid = createGrid(NUMCELLS)
+populateGrid(cell_grid)               
 
-def gridToRgbArrey(grid):
-    return np.array([[grid[i][j].RGB() if grid[i][j] is not None else [0,0,0] for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)]).astype(np.uint8)
-
-def gridToVegetobArrey(grid):
-    return np.array([[grid[i][j].vegetobDensity if grid[i][j] is not None else 0 for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
-
-# Create the function that will be called to update the display
-def update(frame):
-    # Update the animal populations
-    for i in range(planisuss_constants.NUMCELLS):
-        for j in range(planisuss_constants.NUMCELLS):
-            if cell_grid[i][j] is not None: # check only land cells
+#ANIMATION
+def update(frame,grid):
+    for i in range(NUMCELLS):
+        for j in range(NUMCELLS):
+            if grid[i][j] is not None: # check only land cells
                 # Update the vegetob density
-                cell = cell_grid[i][j]
+                cell = grid[i][j]
 
-                cell.vegetobDensity += planisuss_constants.GROWING
+                cell.vegetobDensity += GROWING
                 if cell.vegetobDensity > 100:
                     cell.vegetobDensity = 100
 
-                #get the grid of the neighborhood
-                neighborhood = np.array([[cell_grid[i+ii][j+jj] if cell_grid[i+ii][j+jj] is not None else 0 for jj in range(-planisuss_constants.NEIGHBORHOOD,planisuss_constants.NEIGHBORHOOD+1)] for ii in range(-planisuss_constants.NEIGHBORHOOD,planisuss_constants.NEIGHBORHOOD+1)])
-                
-                if i == len(cell_grid)-2 and j == len(cell_grid)-2:
-                    print(neighborhood.astype(str))
-
-    # Update the display
-    im.set_data(gridToVegetobArrey(cell_grid))
+    im.set_data(getRgbGtidByCellType(grid, "vegetob_grid"))
+    # im1.set_data(getRgbGtidByCellType(cell_grid, "erbast_grid"))
+    # im2.set_data(getRgbGtidByCellType(cell_grid, "carviz_grid"))
+    im3.set_data(getRgbGtidByCellType(grid, "rgb_grid"))
     return [im]
 
-# Create the display
-# rgb_grid = gridToRgbArrey(cell_grid)
 
-vegetob_grid = np.array([[cell_grid[i][j].vegetobDensity if cell_grid[i][j] is not None else 0 for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
-# carviz_grid = np.array([[len(cell_grid[i][j].pride) if cell_grid[i][j] is not None else 0 for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
-# erbast_grid = np.array([[len(cell_grid[i][j].herd) if cell_grid[i][j] is not None else 0 for j in range(planisuss_constants.NUMCELLS)] for i in range(planisuss_constants.NUMCELLS)])
+vegetob_grid = getRgbGtidByCellType(cell_grid, "vegetob_grid")
 
+#PLOT SETUP
 fig, ax = plt.subplots(2,2)
+ax[0][0].set_title("Vegetob Density")
 im = ax[0][0].imshow(vegetob_grid, cmap='Greens', vmax=100, vmin=0)
+ax[0][1].set_title("Erbast Density")
+im1 = ax[0][1].imshow(getRgbGtidByCellType(cell_grid, "erbast_grid"), cmap='Blues', vmax=100, vmin=0)
+ax[1][0].set_title("Carviz Density")
+im2 = ax[1][0].imshow(getRgbGtidByCellType(cell_grid, "carviz_grid"), cmap='Reds', vmax=100, vmin=0)
+ax[1][1].set_title("Overall Density")
+im3 = ax[1][1].imshow(getRgbGtidByCellType(cell_grid, "rgb_grid"))
+#ANIMATION
+# anim = animation.FuncAnimation(fig, update, frames=1, interval=1000, blit=True)
+anim = animation.FuncAnimation(fig, update, fargs=(cell_grid,), frames=1, interval=1000, blit=True)
 
-anim = animation.FuncAnimation(fig, update, frames=1, interval=1000, blit=True)
+# #STOP BUTTON
+# ax_pause = plt.axes((0.8, 0, 0.20, 0.085))
+# bpause = Button(ax_pause, 'Stop')
+# bpause.on_clicked(anim.pause)
 
-ax_pause = plt.axes([0.8, 0, 0.20, 0.085]) # type: ignore
-bpause = Button(ax_pause, 'Stop')
-
-def pause(event):
-    anim.pause()
-
-bpause.on_clicked(pause)
 
 plt.show()
